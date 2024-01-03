be23bbb6c81aacf5d43f42ef13a3ec314da2c724
Alwaysenchanted <tanqinhan@zju.edu.cn>
2024-01-03 10:21:48 -0500

    change top module in jasper

--- fv_jasper/base_BOOM.tcl
+++ fv_jasper/base_BOOM.tcl
@@ -1,8 +1,8 @@
-analyze -sva ./fv_jasper/verilog/BOOM.v ./fv_jasper/verilog/BOOM_mem.v ./fv_jasper/verilog/plusarg_reader.v ./asic/sim/parafuzz.sv
+analyze -sva ./fv_jasper/verilog/TestHarness.v ./fv_jasper/verilog/BOOM.v ./fv_jasper/verilog/BOOM_mem.v ./fv_jasper/verilog/plusarg_reader.v ./asic/sim/parafuzz.sv
 
 check_spv -init
 
-elaborate -top StarshipSimTop -bbox_m StarshipROM
+elaborate -top TestHarness -bbox_m StarshipROM
 
 clock clock
 reset reset -non_resettable_regs 0

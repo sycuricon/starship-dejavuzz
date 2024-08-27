7e047b3b4795092b029be9d0a27af75b43c6cadb
zyy <1076192792@QQ.com>
2024-08-27 12:05:25 -0400

    add change for specdoc

--- asic/sim/variant/rob_sync.boom.v
+++ asic/sim/variant/rob_sync.boom.v
@@ -2,6 +2,7 @@
 `define DUT_ROB_ENQ_INST_0      `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_boom_tile.core.rob.io_enq_uops_0_debug_inst
 `define DUT_ROB_DEQ_EN_0        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_boom_tile.core.rob.io_commit_valids_0
 `define DUT_ROB_DEQ_INST_0      `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_boom_tile.core.rob.io_commit_uops_0_debug_inst
+`define DUT_ROB_DEQ_PC_0        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_boom_tile.core.rob.io_commit_uops_0_debug_pc
 
 `define VNT_ROB_ENQ_EN_0        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_boom_tile.core.rob.io_enq_valids_0
 `define VNT_ROB_ENQ_INST_0      `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_boom_tile.core.rob.io_enq_uops_0_debug_inst
@@ -10,6 +11,9 @@
 
 `define IS_DUT 1
 `define IS_VNT 0
+
+reg [63:0] last_pc = 0;
+reg maybe_deadlock = 0;
 
 always @(posedge clock) begin
   if (!reset) begin
@@ -20,5 +24,21 @@ always @(posedge clock) begin
       event_handler(`VNT_ROB_ENQ_EN_0, `VNT_ROB_ENQ_INST_0, "ENQ", 0, `IS_VNT);
       event_handler(`VNT_ROB_DEQ_EN_0, `VNT_ROB_DEQ_INST_0, "DEQ", 0, `IS_VNT);
     `endif
+
+    if (`DUT_ROB_DEQ_EN_0) begin
+      last_pc <= `DUT_ROB_DEQ_PC_0;
+    end
+
+    if (`DUT_ROB_DEQ_EN_0 && `DUT_ROB_DEQ_PC_0 == last_pc) begin
+      maybe_deadlock <= 1;
+    end
+    else if (`DUT_ROB_DEQ_EN_0 && `DUT_ROB_DEQ_PC_0 != last_pc) begin
+      maybe_deadlock <= 0;
+    end
+
+    if (`DUT_ROB_DEQ_EN_0 && `DUT_ROB_DEQ_PC_0 == last_pc && maybe_deadlock) begin
+      $finish();
+    end
+
   end
 end

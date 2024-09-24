d5c0f1935e4df8e6e1bbbf1f4b4c1ac68c335c15
zyy <1076192792@QQ.com>
2024-09-24 01:07:44 -0400

    add early exit switch

--- asic/sim/variant/rob_sync.boom.v
+++ asic/sim/variant/rob_sync.boom.v
@@ -1,5 +1,6 @@
 `define DUT_ROB_ENQ_EN_0        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_boom_tile.core.rob.io_enq_valids_0
 `define DUT_ROB_ENQ_INST_0      `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_boom_tile.core.rob.io_enq_uops_0_debug_inst
+`define DUT_ROB_ENQ_PC_0        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_boom_tile.core.rob.io_enq_uops_0_debug_pc
 `define DUT_ROB_DEQ_EN_0        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_boom_tile.core.rob.io_commit_valids_0
 `define DUT_ROB_DEQ_INST_0      `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_boom_tile.core.rob.io_commit_uops_0_debug_inst
 `define DUT_ROB_DEQ_PC_0        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_boom_tile.core.rob.io_commit_uops_0_debug_pc
@@ -12,8 +13,19 @@
 `define IS_DUT 1
 `define IS_VNT 0
 
+`define EARLY_EXIT_PC 64'h80600000
+
 reg [63:0] last_pc = 0;
 reg maybe_deadlock = 0;
+reg early_exit = 0;
+
+initial begin
+  void'($value$plusargs("early_exit=%d", early_exit));
+end
+
+wire can_early_exit;
+assign can_early_exit = (`DUT_ROB_ENQ_PC_0 == `EARLY_EXIT_PC)&early_exit;
+reg [3:0] exit_count = 0;
 
 always @(posedge clock) begin
   if (!reset) begin
@@ -36,8 +48,21 @@ always @(posedge clock) begin
       maybe_deadlock <= 0;
     end
 
-    if (`DUT_ROB_DEQ_EN_0 && `DUT_ROB_DEQ_PC_0 == last_pc && maybe_deadlock) begin
-      $finish();
+    if((can_early_exit | maybe_deadlock) && tsx_done == 1'b0)begin
+      tsx_done <= 1'b1;
+      if(can_early_exit)begin
+        $display("early exit");
+      end else begin
+        $display("may dead loop");
+      end
+    end
+
+    if (`DUT_ROB_DEQ_EN_0 && `DUT_ROB_DEQ_PC_0 == last_pc && tsx_done) begin
+      exit_count <= exit_count + 4'b1;
+    end
+
+    if(exit_count == 4'h15)begin
+      $finish;
     end
 
   end
